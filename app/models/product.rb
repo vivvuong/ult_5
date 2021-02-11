baa14bd75f2d528070647532aa98e7267bf2263a
Vivian <vivian.vuong@live.ca>
2021-02-11 03:41:49 -0600

    Added validation to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :title, :stock_quantity, presence: true
+    validates :stock_quantity, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+products_bomb = Product.create(title: "plsdontwork")
+
+if(products_bomb.valid?)
+    # do something
+else
+    puts "Invalid Product"
+end
