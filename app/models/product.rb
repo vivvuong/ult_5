3438937c61bb5183b5b8c5c6ba53076b24d073ed
Vivian <vivian.vuong@live.ca>
2021-02-11 07:40:38 -0600

    Added Category model, imported CSV data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+
     validates :title, :stock_quantity, :price,  presence: true
     validates :stock_quantity, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,53 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require "csv"
 
-$i = 0
-$num = 676
 
-while $i < $num  do
-    product_new = Product.create(title: Faker::Commerce.product_name, stock_quantity: Faker::Number.between(from: 1, to: 10), price: Faker::Commerce.price)
-    $i +=1
+filename = Rails.root.join("db/products.csv")
+puts "Loading in Products from: #{filename}."
 
-    if(product_new.valid?)
-        puts "Product added"
+csv_data = File.read(filename)
+
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+
+products.each do |p|
+    category = Category.find_or_create_by(name: p["category"])
+
+    if category.valid?
+        product = category.products.create(
+            title:              p["name"],
+            description:        p["description"],
+            price:              p["price"],
+            stock_quantity:     p["stock quantity"]
+        )
+
+        puts "Invalid product #{m['name']}" unless product&.valid?
     else
-        puts "Invalid product"
+        puts "Invalid category: #{p["category"]} for product #{p["name"]}"
     end
-
 end
+
+puts "Created #{Product.count} movies."
+puts "Created #{Category.count} categories."
+
+# old code using faker
+
+# $i = 0
+# $num = 676
+
+# while $i < $num  do
+#     product_new = Product.create(title: Faker::Commerce.product_name, stock_quantity: Faker::Number.between(from: 1, to: 10), price: Faker::Commerce.price)
+#     $i +=1
+
+#     if(product_new.valid?)
+#         puts "Product added"
+#     else
+#         puts "Invalid product"
+#     end
+
+# end
 
 # first product starts at index 2 becauise i deleted 
 # the first product i put in and didn't reset the numbers. :(
